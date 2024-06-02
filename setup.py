f343863327f72761ac83abc648e1e9cd0be0b12d
F4de78 <casazucca10@gmail.com>
2024-06-02 11:30:39 +0200

    fix: cython setup

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 from Cython.Build import cythonize
-
+import numpy
 #cython: language_level=3str
 
 setup(
     name='Zero-knowledge memory',
     ext_modules=cythonize('cython_bidirectional_hashes.pyx', language_level='3str'),
     zip_safe=False,
+    include_dirs=[numpy.get_include()]
 )
